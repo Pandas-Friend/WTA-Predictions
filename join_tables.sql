3e8bcd92c18a9c9e9f753ea1fc08ede60c6459b9
Arelys Rosado <80235884+arelysrsd87@users.noreply.github.com>
2021-08-09 19:57:59 -0500

    3

--- join_tables.sql
+++ join_tables.sql
@@ -1,14 +1,12 @@
-select * from matches;
-select * from rankings;
-select * from players;
-
-select ranking
+select p.first_name, p.last_name, p.country, r.ranking, r.date
 into player_ranking
-from rankings as r
-left join players as p 
-on p.player_id = r.player_id
-order by ranking;
+from players as p
+left join rankings as r
+on r.player_id = p.player_id
+order by date;
 
 select * from player_ranking;
+
+
 
 
