4e7556ae3a1cc2fb8d5099e62fb02bf944a2b5cb
Arelys Rosado <80235884+arelysrsd87@users.noreply.github.com>
2021-08-09 20:00:16 -0500

    4

--- join_tables.sql
+++ join_tables.sql
@@ -1,4 +1,4 @@
-select p.first_name, p.last_name, p.country, r.ranking, r.date
+select r.ranking, p.first_name, p.last_name, p.country, r.date
 into player_ranking
 from players as p
 left join rankings as r
@@ -6,6 +6,7 @@ on r.player_id = p.player_id
 order by date;
 
 select * from player_ranking;
+drop table player_ranking; 
 
 
 
