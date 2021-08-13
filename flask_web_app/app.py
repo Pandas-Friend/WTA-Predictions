97c93d8e9cccce347be8c5f6e095a481ada49b9c
Linda Berez <lberez@Lindas-iMac.attlocal.net>
2021-08-13 16:20:49 -0500

    drop down menus populating

--- flask_web_app/app.py
+++ flask_web_app/app.py
@@ -1,85 +1,94 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask import request 
+
 
 app = Flask(__name__)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:GoBrewers12@localhost:5432/car_api"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:GoBrewers12@localhost:5432/WTA"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class CarsModel(db.Model):
-    __tablename__ = 'cars'
+class players(db.Model):
+    __tablename__ = 'player_ranking'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    model = db.Column(db.String())
-    doors = db.Column(db.Integer())
+    ranking = db.Column(db.Integer)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    country = db.Column(db.String())
+    date = db.Column(db.Integer())
+    id = db.Column(db.Integer(), primary_key=True)
+    full_name = db.Column(db.String())
 
-    def __init__(self, name, model, doors):
-        self.name = name
-        self.model = model
-        self.doors = doors
+    def __init__(self, ranking, first_name, last_name, country, date, id, full_name):
+        self.ranking = ranking
+        self.first_name = first_name
+        self.last_name = last_name
+        self.country = country
+        self.date = date
+        self.id = id
+        self.full_name = full_name
 
     def __repr__(self):
-        return f"<Car {self.name}>"
+        return f"<Player {self.name}>"
 
 @app.route('/')
 def hello():
     return {"hello": "world"}
 
-@app.route('/cars', methods=['POST', 'GET'])
-def handle_cars():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
-            db.session.add(new_car)
-            db.session.commit()
-            return {"message": f"car {new_car.name} has been created successfully."}
-        else:
-            return {"error": "The request payload is not in JSON format"}
+@app.route("/input")
+def input():
+    #playerList = db.execute("SELECT DISTINCT first_name, last_name FROM player_ranking WHERE ranking < 150 ORDER BY last_name")
+    return render_template("input.html", players = players.query.with_entities(players.full_name, players.last_name).distinct().where(players.ranking < 150).order_by(players.last_name))
 
-    elif request.method == 'GET':
-        cars = CarsModel.query.all()
-        results = [
-            {
-                "name": car.name,
-                "model": car.model,
-                "doors": car.doors
-            } for car in cars]
+# @app.route('/cars', methods=['POST', 'GET'])
+# def handle_cars():
+#     if request.method == 'POST':
+#         if request.is_json:
+#             data = request.get_json()
+#             new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
+#             db.session.add(new_car)
+#             db.session.commit()
+#             return {"message": f"car {new_car.name} has been created successfully."}
+#         else:
+#             return {"error": "The request payload is not in JSON format"}
 
-        return {"count": len(results), "cars": results}
+#     elif request.method == 'GET':
+#         cars = CarsModel.query.all()
+#         results = [
+#             {
+#                 "name": car.name,
+#                 "model": car.model,
+#                 "doors": car.doors
+#             } for car in cars]
 
-@app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
-def handle_car(car_id):
-    car = CarsModel.query.get_or_404(car_id)
+#         return {"count": len(results), "cars": results}
 
-    if request.method == 'GET':
-        response = {
-            "name": car.name,
-            "model": car.model,
-            "doors": car.doors
-        }
-        return {"message": "success", "car": response}
+# @app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
+# def handle_car(car_id):
+#     car = CarsModel.query.get_or_404(car_id)
 
-    elif request.method == 'PUT':
-        data = request.get_json()
-        car.name = data['name']
-        car.model = data['model']
-        car.doors = data['doors']
-        db.session.add(car)
-        db.session.commit()
-        return {"message": f"car {car.name} successfully updated"}
+#     if request.method == 'GET':
+#         response = {
+#             "name": car.name,
+#             "model": car.model,
+#             "doors": car.doors
+#         }
+#         return {"message": "success", "car": response}
 
-    elif request.method == 'DELETE':
-        db.session.delete(car)
-        db.session.commit()
-        return {"message": f"Car {car.name} successfully deleted."}
+#     elif request.method == 'PUT':
+#         data = request.get_json()
+#         car.name = data['name']
+#         car.model = data['model']
+#         car.doors = data['doors']
+#         db.session.add(car)
+#         db.session.commit()
+#         return {"message": f"car {car.name} successfully updated"}
+
+#     elif request.method == 'DELETE':
+#         db.session.delete(car)
+#         db.session.commit()
+#         return {"message": f"Car {car.name} successfully deleted."}
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
