d7ebcfbe719d9bc03ad973628a42e40aea9fa036
LBerez <lberez@mac.com>
2021-08-20 21:44:39 -0500

    adding updated files

--- flask_web_app/app.py
+++ flask_web_app/app.py
@@ -7,13 +7,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:GoBrewers12@local
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 def get_dropdown_values():
-    player_values = players.query.with_entities(players.full_name).distinct().where(players.ranking < 150).order_by(players.last_name)
+    player_values = players.query.with_entities(players.full_name).distinct().where(
+        players.ranking < 150).order_by(players.last_name)
     return player_values
 
+
 def get_dates():
-    dates = matches.query.with_entities(matches.tourney_date).distinct().where(matches.tourney_date.isnot(None)).order_by(matches.tourney_date)
+    dates = players.query.with_entities(players.ranking_dates).distinct().where(
+        players.ranking_dates.isnot(None)).order_by(players.ranking_dates)
     return dates
+
+def get_player(player, dates):
+    return players.query.with_entities(players.ranking).where(players.full_name==player).where(players.ranking_dates==dates)
 
 class players(db.Model):
     __tablename__ = 'player_ranking'
@@ -22,21 +29,22 @@ class players(db.Model):
     first_name = db.Column(db.String())
     last_name = db.Column(db.String())
     country = db.Column(db.String())
-    date = db.Column(db.Integer())
-    id = db.Column(db.Integer(), primary_key=True)
+    ranking_dates = db.Column(db.Integer())
+    ident = db.Column(db.Integer(), primary_key=True)
     full_name = db.Column(db.String())
 
-    def __init__(self, ranking, first_name, last_name, country, date, id, full_name):
+    def __init__(self, ranking, first_name, last_name, country, ranking_dates, ident, full_name):
         self.ranking = ranking
         self.first_name = first_name
         self.last_name = last_name
         self.country = country
-        self.date = date
-        self.id = id
+        self.ranking_dates = ranking_dates
+        self.id = ident
         self.full_name = full_name
 
     def __repr__(self):
         return f"<Player {self}>"
+
 
 class matches(db.Model):
     __tablename__ = 'flask_matches'
@@ -61,22 +69,21 @@ class matches(db.Model):
     def __repr__(self):
         return f"<Matches {self}>"
 
+
 @app.route('/_process_data')
 def process_data():
     selected_player_a = request.args.get('selected_player_a', type=str)
     selected_player_b = request.args.get('selected_player_b', type=str)
     selected_surface = request.args.get('selected_surface', type=str)
-    selected_date = request.args.get('selected_date', type=str)
-
-    # process the two selected values here and return the response; here we just create a dummy string
-
-    return jsonify(random_text="You selected {} and {} for date of {} on a {} surface".format(selected_player_a, selected_player_b, selected_date, selected_surface))
+    selected_date = request.args.get('selected_date', type=str)	
+    player1 = get_player(selected_player_a, selected_date)
+    return jsonify(random_text="You selected {} and {} for date of {} on a {} surface {}".format(selected_player_a, selected_player_b, selected_date, selected_surface, player1))
 
 @app.route("/")
 def index():
-      values = get_dropdown_values()
-      date_value = get_dates()
-      return render_template("index.html", player_a=values, player_b=values, dates=date_value)
+    values = get_dropdown_values()
+    date_value = get_dates()
+    return render_template("index.html", player_a=values, player_b=values, dates=date_value)
 
 
 if __name__ == '__main__':
