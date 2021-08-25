c89648e754b615426f957ddf685d01b529c26855
Linda Berez <lberez@Lindas-iMac.attlocal.net>
2021-08-25 18:56:25 -0500

    updating join_tables.sql

--- SQL/join_tables.sql
+++ SQL/join_tables.sql
@@ -1,13 +1,20 @@
-select r.ranking, p.first_name, p.last_name, p.country, r.date
-into player_ranking
-from players as p
-left join rankings as r
-on r.player_id = p.player_id
-order by date;
+SELECT r.ranking, p.first_name, p.last_name, p.country, r.date
+INTO player_ranking
+FROM players AS p
+LEFT JOIN rankings AS r
+ON r.id = p.player_id
+ORDER BY date;
 
-select * from player_ranking;
-drop table player_ranking; 
+ALTER TABLE player_ranking ADD COLUMN ID SERIAL PRIMARY KEY;
+ALTER TABLE player_ranking
+ADD COLUMN full_name varchar;
+UPDATE player_ranking SET full_name = CONCAT(first_name, ' ', last_name)
 
+ALTER TABLE player_ranking
+RENAME COLUMN date TO ranking_dates;
+
+ALTER TABLE player_ranking
+RENAME COLUMN id TO ident
 
 
 
